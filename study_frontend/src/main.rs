f8f38bab2e110c1323a6f9c6aa8d5452272d2747
gschup <georg.schuppe@gmail.com>
2022-09-13 09:55:25 +0200

    send correct request

--- study_frontend/src/main.rs
+++ study_frontend/src/main.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
 use study_shared_types::GameResults;
-use wasm_bindgen::JsCast;
+use wasm_bindgen::{JsCast, JsValue};
 use wasm_bindgen_futures::{spawn_local, JsFuture};
-use web_sys::{Request, RequestInit, RequestMode, Response};
+use web_sys::{Headers, Request, RequestInit, RequestMode, Response};
 
 fn main() {
     App::new()
@@ -11,30 +11,34 @@ fn main() {
         .run();
 }
 
-/// This system prints 'A' key state
 fn keyboard_input_system(keyboard_input: Res<Input<KeyCode>>) {
     if keyboard_input.just_pressed(KeyCode::A) {
         info!("'A' just pressed");
+        let result = GameResults { participant_id: 7 };
+
+        // create request - hacky json encoding
+        let result_json = format!("{{\"participant_id\": {}}}", result.participant_id);
+        let body = JsValue::from_str(&result_json);
+        let headers = Headers::new().unwrap();
+        headers.set("content-type", "application/json").unwrap();
+        let url = format!("http://127.0.0.1:3030/data");
         let mut opts = RequestInit::new();
-        opts.method("GET");
-        opts.mode(RequestMode::NoCors);
-
-        let url = format!("http://127.0.0.1:3030/hello");
-
+        opts.method("POST")
+            .mode(RequestMode::Cors)
+            .body(Some(&body))
+            .headers(&headers);
         let request = Request::new_with_str_and_init(&url, &opts).unwrap();
 
-        request.headers().set("Accept", "text/plain").unwrap();
-
+        // send request
         let window = web_sys::window().unwrap();
         spawn_local(async move {
-            let resp_value = JsFuture::from(window.fetch_with_request(&request))
-                .await
-                .unwrap();
-            assert!(resp_value.is_instance_of::<Response>());
-            let resp: Response = resp_value.dyn_into().unwrap();
-
-            if let Some(resp_string) = resp.as_string() {
-                println!("RESPONSE IS {:?}", resp_string);
+            match JsFuture::from(window.fetch_with_request(&request)).await {
+                Ok(resp_value) => {
+                    assert!(resp_value.is_instance_of::<Response>());
+                    let _resp: Response = resp_value.dyn_into().unwrap();
+                    // TODO: do something with the response
+                }
+                Err(e) => error!("Could not send results: {:?}", e),
             }
         });
     }
