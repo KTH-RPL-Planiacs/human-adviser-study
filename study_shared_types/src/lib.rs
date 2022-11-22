6816f6002eea9cb9da8d7d69e6079a10aa12624d
gschup <georg.schuppe@gmail.com>
2022-11-22 13:04:47 +0100

    expand game results

--- study_shared_types/src/lib.rs
+++ study_shared_types/src/lib.rs
@@ -3,4 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct GameResults {
     pub participant_id: i32,
+    pub human_burgers: u32,
+    pub robot_burgers: u32,
 }
